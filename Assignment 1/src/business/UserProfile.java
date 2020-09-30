13756056196c12d44bcfc1df61912fc8d620a3e5
Jinnan Shao <shao.jin@northeastern.edu>
2020-09-30 20:53:16 +0800

    test git

---
+++ Assignment 1/src/business/UserProfile.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+/**
+ *
+ * @author shaojinnan
+ */
+public class UserProfile {
+    
+    private String name;
+    private String city;
+    private String birthday;
+    private String tel;
+    private String fax;
+    private String email;
+    private String SSN;
+    private String MRN;
+    private String HPBN;
+    private String BAN;
+    private String license;
+    private String vehicle;
+    private String device;
+    private String linkedln;
+    private String internet;
+    private String fingerprints;
+    private String photo;
+    private String ID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getMRN() {
+        return MRN;
+    }
+
+    public void setMRN(String MRN) {
+        this.MRN = MRN;
+    }
+
+    public String getHPBN() {
+        return HPBN;
+    }
+
+    public void setHPBN(String HPBN) {
+        this.HPBN = HPBN;
+    }
+
+    public String getBAN() {
+        return BAN;
+    }
+
+    public void setBAN(String BAN) {
+        this.BAN = BAN;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getLinkedln() {
+        return linkedln;
+    }
+
+    public void setLinkedln(String linkedln) {
+        this.linkedln = linkedln;
+    }
+
+    public String getInternet() {
+        return internet;
+    }
+
+    public void setInternet(String internet) {
+        this.internet = internet;
+    }
+
+    public String getFingerprints() {
+        return fingerprints;
+    }
+
+    public void setFingerprints(String fingerprints) {
+        this.fingerprints = fingerprints;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    
+    
+    
+}
