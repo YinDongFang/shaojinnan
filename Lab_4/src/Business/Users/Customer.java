f9c7801c09f761cc4208231d5d36fd6da58c8819
Jinnan Shao <shao.jin@northeastern.edu>
2020-10-10 22:11:05 +0800

    Customer class

--- Lab_4/src/Business/Users/Customer.java
+++ Lab_4/src/Business/Users/Customer.java
@@ -12,6 +12,31 @@ import java.util.Date;
  *
  * @author AEDSpring2019
  */
-public class Customer {
+public class Customer extends User implements Comparable<Customer>{
     
+    private Date created;
+    
+    public Customer(String password, String userName) {
+        super(password, userName, "CUSTOMER");
+        created = new Date();
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    @Override
+    public int compareTo(Customer o) {
+        return o.getUserName().compareTo(this.getUserName());
+    }
+
+    @Override
+    public String toString() {
+        return getUserName(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public boolean verify(String password){
+        return password.equals(getPassword());
+    }
 }
+
