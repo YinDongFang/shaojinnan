1863935f350aed03fca2759092e0312a2feffe2d
Jinnan Shao <shao.jin@northeastern.edu>
2020-10-10 22:18:39 +0800

    Populate the Customer table

--- Lab_4/src/UserInterface/AdminMainScreen.java
+++ Lab_4/src/UserInterface/AdminMainScreen.java
@@ -121,7 +121,16 @@ public class AdminMainScreen extends javax.swing.JPanel {
             row[1]=s.getDirectory().getProductList().size();
             dtm.addRow(row);
         }
-
+        
+        dtm = (DefaultTableModel)tableCust.getModel();
+        dtm.setRowCount(0);
+        for(User u : admin.getCustDir().getCustomerList()){
+            Customer c = (Customer)u;
+            Object[] row = new Object[dtm.getColumnCount()];
+            row[0]=c;
+            row[1]=c.getCreated();
+            dtm.addRow(row);
+        }
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
